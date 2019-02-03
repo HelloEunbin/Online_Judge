5b70101f6e8b396593d2ecee04d9b0033c07447d
은빈 김 <bin747@donga.ac.kr>
2019-02-03 17:34:53 +0900

    동적프로그래밍 조금 더 연습하장~

---
+++ baekjoon/baekjoon/11052카드구매하기.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int N;
+
+	cin >> N;
+	int card[1001] = { 0, };
+
+	int max;
+	cin >> card[1]; //1장 최대
+	for (int i = 2; i <= N; i++) {
+		cin >> max;
+		for (int j = i-1; j >= i-j; j--) { //2장일 때 최대는 2 or 1+1, 3장일 때 최대는 3 or 2+1(1+1+1은 2의 최대를 알기 때문에 2+1로 됨), 4장일 때 최대는 4,3+1,2+2
+			if (card[j] + card[i - j] > max)
+				max = card[j] + card[i - j];
+		}
+		card[i] = max;
+	}
+	cout << card[N];
+
+	return 0;
+}
