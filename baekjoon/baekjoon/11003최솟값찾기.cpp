60db561fbf15a9fb2a353c8a57d1ea9de3ba2961
은빈 김 <bin747@donga.ac.kr>
2019-02-08 04:43:24 +0900

    cin, cout이 아무래도 시간을 많이 잡아 먹는다. 이럴 땐 printf쓰자! deque에 인덱스도 저장 가능하다.

--- baekjoon/baekjoon/11003최솟값찾기.cpp
+++ baekjoon/baekjoon/11003최솟값찾기.cpp
@@ -1,31 +1,30 @@
-#include<iostream>
+#include<stdio.h>
 #include<deque>
-#define fastIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#pragma warning(disable:4996)
 using namespace std;
 
 int main() {
 
 	int N, L;
 
-	cin >> N >> L;
+	scanf("%d %d", &N, &L);
 
-	deque<int> deque;
+	deque<pair<int,int>> deque; //pair로 하면 인덱스 저장 가능하다. deque는 앞 뒤로 둘 다 빼고 넣고 할 수 있다.
 
 	int a;
-	int cnt = 1;
+	int cnt = 0;
 
 	for (int i = 0; i < N; i++) {
-		cin >> a;
-		if (cnt == L) {
-			deque.pop_front();
-			cnt = 1;
+		scanf("%d", &a);
+		while (!deque.empty()&&a < deque.back().first) { //덱이 empty가 아니고, a가 덱의 가장뒤의 값보다 작으면
+			deque.pop_back(); //a가 더 클때까지 계속 뺀다. 빼다가 empty 될수 있으니 empty조건이 필요하다.
 		}
-		while (!deque.empty()&&a < deque.back()) {
-			deque.pop_back();
-		}
-			deque.push_back(a);
-			cnt++;
-			cout << deque.front() << " ";
+		deque.push_back({ a, i }); //인덱스와 함께 push, ()안에 {}넣어 콤마로 구분 //인덱스는 0부터 시작
+
+		if (deque.front().second == i - L) //덱의 제일 앞 인덱스가 L의 범위 밖일 때 ex) L=3이고 i=3이면 i=1,i=2,i=3만 봐야하는데 front().second=0일때
+			deque.pop_front(); //pop한다.
+
+		printf("%d ", deque.front().first); //제일 앞 값 출력
 
 	}
 
