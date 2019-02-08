cccd2adfd3207e43ec2f37db243950ab4bc0ca55
은빈 김 <bin747@donga.ac.kr>
2019-02-08 15:09:08 +0900

    별찍기 9번

---
+++ baekjoon/baekjoon/별찍기-9.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int num;
+	cin >> num;
+
+	int i, j, k;
+	for (i = 1; i <= num; i++) {
+		for (j = 1; j < i; j++) {
+			cout << " ";
+		}
+		for (k = j; k <= num * 2 - i; k++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	for (i = num - 1; i > 0; i--) {
+		for (j = 1; j < i; j++) {
+			cout << " ";
+		}
+		for (k = j; k <= num * 2 - i; k++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
