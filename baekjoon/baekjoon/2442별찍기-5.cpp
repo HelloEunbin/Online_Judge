88f4a5070340e9df91a748c0180ef714b049fa52
은빈 김 <bin747@donga.ac.kr>
2019-02-08 14:30:22 +0900

    별찍기 5번

--- baekjoon/baekjoon/2442별찍기-5.cpp
+++ baekjoon/baekjoon/2442별찍기-5.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 	int num;
 	cin >> num;
-	for (int i = num; i > 0; i--) { //총 num번 돌리기
+	for (int i = num; i > 0; i--) { //총 5번 돌리기
 		for (int j = i-1; j > 0; j--) { //빈칸은 4,3,2,1순
 			cout << " ";
 		}
